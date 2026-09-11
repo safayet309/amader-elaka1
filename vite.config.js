ab0992a621ad3588b8aae2e22768b6ce8a00d236
Md Safayet Hossan <safayethossain309@gmail.com>
2026-09-11 14:15:50 +0600

    Update Vite configuration for build output

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: "src",
-
   plugins: [react()],
 
   base: "/amader-elaka1/",
 
   build: {
-    outDir: "../",
-    emptyOutDir: false,
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
