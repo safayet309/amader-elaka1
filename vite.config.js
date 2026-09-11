9bb4e93ac486f84ae61351ce5165bc0b88f21b63
Md Safayet Hossan <safayethossain309@gmail.com>
2026-09-11 14:13:52 +0600

    Configure Vite for project structure and build output

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "src",
+
   plugins: [react()],
 
   base: "/amader-elaka1/",
+
+  build: {
+    outDir: "../",
+    emptyOutDir: false,
+  },
 });
